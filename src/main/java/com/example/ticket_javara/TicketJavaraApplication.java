ccece9ae1d19b9ff94e96e52d98d26b92b2be765
Taeguni06 <aqwsde0412@gmail.com>
2026-04-08 16:32:57 +0900

    Initial commit

---
+++ src/main/java/com/example/ticket_javara/TicketJavaraApplication.java
@@ -0,0 +1,13 @@
+package com.example.ticket_javara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketJavaraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketJavaraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ticket_javara/TicketJavaraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ticket_javara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketJavaraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
